9d2a3a44811fd1ea74ca342f7938311d903be8e1
Se-Basstian <p.sebastian.23.11.99@gmail.com>
2025-01-24 20:06:39 -0500

    Primer commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <>
+            <p>Holas</p>
+        </>
+    )
+}
+
+export default App
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,12 @@
+import clsx from 'clsx'
+import type { FC, ReactNode } from 'react'
+
+type Props = {
+    children: ReactNode
+}
+
+const Header: FC<Props> = ({ children }) => {
+    return <header className={clsx('w-full')}>{children}</header>
+}
+
+export default Header
