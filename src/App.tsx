e94861a50e74971537d4ae7a6b41ebd152423a6a
Se-Basstian <p.sebastian.23.11.99@gmail.com>
2025-01-24 23:04:04 -0500

    <Header/> y <Banner/> creado

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Header from './components/layout/Header'
+import Banner from './components/header/Banner'
+
 function App() {
     return (
         <>
-            <p>Holas</p>
+            <Header>
+                <Banner />
+            </Header>
         </>
     )
 }
